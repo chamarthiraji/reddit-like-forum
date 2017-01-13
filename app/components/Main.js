ce1dde9190a884d5a1855d6fb52daf67720cfa68
Rajyalakshmi C <chamarthi226@gmail.com>
2017-01-13 00:09:43 -0600

    intial commit

---
+++ app/components/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link }  from 'react-router';
+
+export default class Main extends Component {
+	render() {
+		console.log("main....");
+		return (
+			<div className="container">
+	      		<div className="jumbotron">
+		        	<h2><strong>Reddit!</strong></h2>
+		        	<Link to="/addData">add Post</Link>
+	      		</div>
+
+		      	<div className="row">
+		        	{/* This code will dump the correct Child Component */}
+		        	{this.props.children}
+		      	</div>
+	    	</div>
+		);
+	}
+}
