8004cc543d4924f24cc3bf50c0f151f1dcae73be
Rajyalakshmi C <chamarthi226@gmail.com>
2017-01-15 13:55:56 -0600

    added heroku plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
+
+const path = require('path')
+const webpack = require('webpack')
+
 module.exports = {
+
   entry: "./app/app.js",
 
   output: {
     filename: "public/bundle.js"
   },
 
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  
   module: {
     loaders: [
       {
