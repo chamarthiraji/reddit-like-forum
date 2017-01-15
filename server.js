c22a7494cc2fdef704321a341db4c2d33b98525d
Rajyalakshmi C <chamarthi226@gmail.com>
2017-01-15 14:05:33 -0600

    port added

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const posts = require('./api/routes/posts');
 const PORT = process.env.PORT || 3000;
 var mongoURI = process.env.MONGODB_URI || "mongodb://localhost/reddit";
 
-
+console.log("PORT: ,mongoURI: ",PORT);
+console.log("mongoURI: ",mongoURI);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
